7631858ec5ccff742e9e0090e68507f40924fcfa
GnomeShift <rainy.top@yandex.ru>
2025-01-13 17:30:19 +0300

    Initial commit

---
+++ KT1/test_KT1.py
@@ -0,0 +1,26 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver import Keys
+from selenium.webdriver.common.by import By
+
+
+@pytest.fixture(scope="function")
+def driver():
+    driver = webdriver.Firefox()
+    driver.implicitly_wait(10)
+    yield driver
+    driver.quit()
+
+
+def test_page_title(driver):
+    driver.get("https://www.google.com")
+    assert driver.title == "Google"
+
+
+def test_search(driver):
+    driver.get("https://www.google.com")
+    search_box = driver.find_element(By.NAME, "q")
+    search_box.send_keys("Selenium")
+    search_box.send_keys(Keys.RETURN)
+
+    assert "Selenium - Поиск в Google" in driver.title
