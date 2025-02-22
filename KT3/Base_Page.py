2ae856275ccb75ecc75863a2e65e356409968ebb
GnomeShift <rainy.top@yandex.ru>
2025-02-22 19:55:20 +0300

    feat: add KT3

---
+++ KT3/Base_Page.py
@@ -0,0 +1,9 @@
+from selenium import webdriver
+
+class BasePage:
+    BASE_URL = "http://192.168.0.225:8081"
+
+    def __init__(self, driver):
+        self.driver = driver
+        self.driver.implicitly_wait(3)
+        self.driver.get(BasePage.BASE_URL)
